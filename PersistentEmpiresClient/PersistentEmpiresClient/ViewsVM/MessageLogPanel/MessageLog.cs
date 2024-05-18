757e4407d00c2dfea1b6f8601b6ce27e9ef0eddf
Snah79 <cendrowski@hotmail.com>
2024-05-18 09:33:16 +0200

    Revert "asd"
    This reverts commit 53c83e1bf6144694d96b89fa44b8f849983b7988.

--- PersistentEmpiresClient/PersistentEmpiresClient/ViewsVM/MessageLogPanel/MessageLog.cs
+++ PersistentEmpiresClient/PersistentEmpiresClient/ViewsVM/MessageLogPanel/MessageLog.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
 
 namespace PersistentEmpiresClient.ViewsVM.MessageLogPanel
