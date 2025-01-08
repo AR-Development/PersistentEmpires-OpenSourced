aebff6a1173fd11511f42fcefd2441382db93fbe
Michal Cendrowski <cendrowski@hotmail.com>
2025-01-08 10:16:41 +0000

    Wounding => TryToSheathWeaponInHand fix

--- PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/WoundingBehavior.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/WoundingBehavior.cs
@@ -134,7 +134,7 @@ namespace PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors
                     var peer = GameNetwork.NetworkPeers.Where(x => x.VirtualPlayer?.ToPlayerId() == player).FirstOrDefault();
                     if (peer?.ControlledAgent != null)
                     {
-                        if (!ItemsWhichCanBeUsedByWounded.Contains(peer.ControlledAgent.WieldedWeapon.Item?.StringId) || !AgentHungerBehavior.Instance.Eatables.Any(x=> x.Item == peer.ControlledAgent.WieldedWeapon.Item))
+                        if (!string.IsNullOrEmpty(peer.ControlledAgent.WieldedWeapon.Item?.StringId) && (!ItemsWhichCanBeUsedByWounded.Contains(peer.ControlledAgent.WieldedWeapon.Item?.StringId) || !AgentHungerBehavior.Instance.Eatables.Any(x=> x.Item == peer.ControlledAgent.WieldedWeapon.Item)))
                         {
                             peer.ControlledAgent?.TryToSheathWeaponInHand(Agent.HandIndex.MainHand, Agent.WeaponWieldActionType.Instant);
                         }
