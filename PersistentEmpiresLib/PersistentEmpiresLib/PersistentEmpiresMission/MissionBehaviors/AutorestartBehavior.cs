b9750ef823d3dfe9dc68b69f1dfb1466c2e15163
Michal Cendrowski <cendrowski@hotmail.com>
2025-01-09 09:39:01 +0000

    Fix for AutorestartBehavior

--- PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/AutorestartBehavior.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/AutorestartBehavior.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
@@ -11,19 +13,21 @@ namespace PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors
         public int IntervalHour = 24;
 
         private long _restartAt = 0;
-        private readonly List<(int Minutes, string Message, string DebugMessage)> _checkpoints;
+        private readonly Dictionary<int, (string Message, string DebugMessage)> _checkpoints;
 
         public AutorestartBehavior()
         {
-            _checkpoints = new List<(int, string, string)>
+            _checkpoints = new Dictionary<int, (string Message, string DebugMessage)>()
             {
-                (180, "3 hours", "Server will be restarted in 3 hours."),
-                (60, "1 hour", "Server will be restarted in 1 hour."),
-                (30, "30 minutes", "Server will be restarted in 30 minutes."),
-                (15, "15 minutes", "Server will be restarted in 15 minutes."),
-                (5, "5 minutes", "Server will be restarted in 5 minutes."),
-                (1, "1 minute", "Server will be restarted in 1 minute."),
-                (0, "10 seconds", "Server will be restarted in 10 seconds.")
+                //{ 180, ("3 hours", "Server will be restarted in 3 hours.") },
+                //{3600, ("1 hour", "Server will be restarted in 1 hour.") },
+                {1800, ("30 minutes", "Server will be restarted in 30 minutes.") },
+                {900, ("15 minutes", "Server will be restarted in 15 minutes.") },
+                {300, ("5 minutes", "Server will be restarted in 5 minutes.") },
+                {60, ("1 minute", "Server will be restarted in 1 minute.") },
+                {30, ("30 seconds", "Server will be restarted in 30 seconds.") },
+                {20, ("20 seconds", "Server will be restarted in 20 seconds.") },
+                {10, ("10 seconds", "Log the fuck off before you lose yo shit to the final messages.") }
             };
         }
 
@@ -40,27 +44,24 @@ namespace PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors
         public override void OnMissionTick(float dt)
         {
             base.OnMissionTick(dt);
+
             if (_restartAt == 0)
             {
                 _restartAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds() + (IntervalHour * 3600);
                 return;
             }
 
-            long remainingSeconds = _restartAt - DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            int remainingSeconds = (int)(_restartAt - DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+
             if (IsActive && remainingSeconds <= 0)
             {
                 throw new Exception("Server auto restart.");
             }
 
-            int remainingMinutes = (int)(remainingSeconds / 60);
-            foreach (var checkpoint in _checkpoints)
+            if (_checkpoints.ContainsKey(remainingSeconds))
             {
-                if (remainingMinutes <= checkpoint.Minutes && !_checkpoints.Contains(checkpoint))
-                {
-                    InformationComponent.Instance.BroadcastQuickInformation(checkpoint.Message);
-                    Debug.Print(checkpoint.DebugMessage);
-                    _checkpoints.Add(checkpoint);
-                }
+                InformationComponent.Instance.BroadcastQuickInformation(_checkpoints[remainingSeconds].DebugMessage);
+                Debug.Print(_checkpoints[remainingSeconds].DebugMessage);
             }
         }
     }
