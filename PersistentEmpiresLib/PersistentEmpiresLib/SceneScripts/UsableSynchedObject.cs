acd8d32bf5d205017dce315b0665045920e93806
Michal Cendrowski <cendrowski@hotmail.com>
2025-01-11 14:21:38 +0000

    PE_UsableFromDistance for PE_UsableSynchedObject

--- PersistentEmpiresLib/PersistentEmpiresLib/SceneScripts/UsableSynchedObject.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/SceneScripts/UsableSynchedObject.cs
@@ -6,8 +6,19 @@ using TaleWorlds.MountAndBlade;
 
 namespace PersistentEmpiresLib.SceneScripts
 {
-    public abstract class PE_UsableSynchedObject : UsableMissionObject
+    public abstract class PE_UsableSynchedObject : PE_UsableFromDistance
     {
+        public override void OnUse(Agent userAgent)
+        {
+            if (!base.IsUsable(userAgent))
+            {
+                userAgent.StopUsingGameObjectMT(false);
+                return;
+            }
+
+            base.OnUse(userAgent);
+        }
+
         public void AddBodyFlagsSynchedPE(BodyFlags flags, bool applyToChildren = true)
         {
             if ((base.GameEntity.BodyFlag & flags) != flags)
